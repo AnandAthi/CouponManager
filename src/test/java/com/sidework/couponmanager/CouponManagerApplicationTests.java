87869ee01925a676138ceba8662205f4613c6e89
Anand <anand.a8807@gmail.com>
2020-10-11 22:43:02 -0400

    Initial commit

---
+++ src/test/java/com/sidework/couponmanager/CouponManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sidework.couponmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
